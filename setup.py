6df40c7002b46b3aa0297234d703026dbba85eb3
Walter Doekes <walter+github@wjd.nu>
2015-02-02 17:12:19 +0100

    build: Fix setup.py to work with the new path.

--- setup.py
+++ setup.py
@@ -18,22 +18,23 @@ for root, dirs, files in os.walk(os.path.dirname(__file__) or '.'):
     new_root = root[striplen:]
     if '__init__.py' in files:
         if '.' not in new_root:
-            packages.append('osso.' + new_root.replace('/', '.'))
+            packages.append(new_root.replace('/', '.'))
 
     for file in files:
         if (os.path.splitext(file)[1] in ('.html', '.xml', '.js',
                                           '.po', '.mo', '.diff') or
-            file in ('COPYING', 'README', 'Makefile') or
-            file.startswith('LC_')):  # LC_locale_stuff
-            data.append(os.path.join(new_root, file))
+                file in ('COPYING', 'README', 'Makefile') or
+                file.startswith('LC_')):  # LC_locale_stuff
+            # Drop osso/ when appending.
+            data.append(os.path.join(new_root, file)[5:])
 
 # Remove some?
 packages = [i for i in packages if not i.startswith('osso.cms')]
-data = [i for i in data if not i.startswith('cms/')]
+data = [i for i in data if not i.startswith('cms/')]  # relative to osso/
 packages = [i for i in packages if not i.startswith('osso.search')]
-data = [i for i in data if not i.startswith('search/')]
-#packages = [i for i in packages if not i.startswith('osso.sms')]
-#data = [i for i in data if not i.startswith('sms/')]
+data = [i for i in data if not i.startswith('search/')]  # relative to osso/
+# packages = [i for i in packages if not i.startswith('osso.sms')]
+# data = [i for i in data if not i.startswith('sms/')]
 
 
 #####################
@@ -58,7 +59,7 @@ def _get_data_files_HACK(self):
 
         # Length of path to strip from found files
         # FIX IS HERE!
-        #plen = len(src_dir)+1
+        # plen = len(src_dir)+1
         if src_dir:
             plen = len(src_dir) + 1
         else:
@@ -83,41 +84,33 @@ build_py._get_data_files = _get_data_files_HACK
 setup(
     # In need of a better name.
     name='osso-djuty',
-    # Trying to use a PEP386 and distutils.version.StrictVersion compatible
-    # versioning scheme here: 0.2a sorts before 0.2 and will mean
-    # not-exactly-0.2-yet.
+    # Trying to use a PEP386 and distutils.version.StrictVersion
+    # compatible versioning scheme here: 0.2a sorts before 0.2 and will
+    # mean not-exactly-0.2-yet.
     version='0.0.1a',  # perpetual dev-mode
-    # Put everything in the "osso" dir. We may want to rename that at some
-    # point.
-    package_dir={'osso': ''},
+    # Which files?
     packages=packages,
-    package_data={'': data + [
-        'core/middleware-fail2ban.diff',
-        'core/tests-data/to_linear_text.html',
-        'core/tests-data/to_linear_text.txt',
-        'sms/sql/textmessage.mysql.sql',
-        'sms/sql/textmessage.postgresql_psycopg2.sql',
-    ]},
-    # Explicitly set to empty, but that doesn't matter. It breaks anyway :(
+    package_data={'osso': data},  # cheat and let all data belong to osso pkg
     data_files=(),
+    # Descriptions.
     description='OSSO-Djuty Django utility lib',
     long_description=('Various useful tools, partly used for development '
                       'with Django and partly useful without.'),
     author='OSSO B.V.',
     author_email='info+osso-djuty@osso.nl',
     url='https://github.com/ossobv/osso-djuty',
-    #license='Unknown',
-    #platforms=('linux',),
-    #classifiers=[
-    #    'Development Status :: 4 - Beta',
-    #    'Intended Audience :: Developers',
-    #    ('License :: OSI Approved :: GNU General Public License v3 or later '
-    #     '(GPLv3+)'),
-    #    'Operating System :: MacOS :: MacOS X',
-    #    'Operating System :: POSIX :: Linux',
+    # license='Unknown',
+    # platforms=('linux',),
+    # classifiers=[
+    #     'Development Status :: 4 - Beta',
+    #     'Intended Audience :: Developers',
+    #     ('License :: OSI Approved :: GNU General Public License v3 or later '
+    #      '(GPLv3+)'),
+    #     'Operating System :: MacOS :: MacOS X',
+    #     'Operating System :: POSIX :: Linux',
     #    'Programming Language :: Python :: 2',
-    #    'Topic :: Software Development :: Libraries',
-    #],
+    #     'Topic :: Software Development :: Libraries',
+    # ],
 )
 
 # vim: set ts=8 sw=4 sts=4 et ai tw=79:
