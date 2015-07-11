98e6e4f7f4a227d6c5c60f62b5f5f89db1072ac1
Walter Doekes <walter+github@wjd.nu>
2015-07-11 10:29:03 +0200

    core: Improve strftime tests.
    * Move tests to test_templatetags, the canonical naming scheme which
      allows test_* searches to find the tests.
    * Add language specific tests to the strftime tests.
    * Add hardcoded test to strftime tests.

---
+++ osso/core/tests/test_templatetags.py
@@ -0,0 +1,47 @@
+# vim: set ts=8 sw=4 sts=4 et ai:
+import datetime
+
+from django.conf import settings
+from django.test import TestCase
+from django.utils.translation import activate, get_language
+
+from ..templatetags.core import hourstohuman, strftime
+
+
+class CoreTemplatetagsTestCase(TestCase):
+    def test_hourstohuman(self):
+        self.assertEqual(hourstohuman('0.25'), '0:15')
+        self.assertEqual(hourstohuman('3.5'), '3:30')
+        self.assertEqual(hourstohuman('3.5085'), '3:30:30')
+
+    def test_strftime(self):
+        self.assertEqual(strftime(datetime.datetime(2015, 2, 27, 1, 2, 3, 4),
+                                  '%y%m%d-%H%M%S'),
+                         '150227-010203')
+
+    def test_strftime_auto(self):
+        orig_language = get_language()
+        orig_l10n = settings.USE_L10N
+
+        settings.USE_L10N = True
+        try:
+            activate('en')
+            self.assertEqual(strftime(datetime.datetime(2015, 2, 27)),
+                             '2015-02-27 00:00:00')
+            self.assertEqual(strftime(datetime.date(2015, 2, 27)),
+                             '2015-02-27')
+            self.assertEqual(strftime(datetime.time(22, 12, 10)),
+                             '22:12:10')
+
+            activate('nl')
+            self.assertEqual(strftime(datetime.datetime(2015, 2, 27)),
+                             '27-02-2015 00:00:00')
+            self.assertEqual(strftime(datetime.date(2015, 2, 27)),
+                             '27-02-2015')
+
+            activate('hu')
+            self.assertEqual(strftime(datetime.time(22, 12, 10)),
+                             '22.12.10')
+        finally:
+            activate(orig_language)
+            settings.USE_L10N = orig_l10n
