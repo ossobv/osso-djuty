403c7bce43ee244f8f7717b4a4289d7587847188
Harm Geerts <hgeerts@osso.nl>
2017-01-18 15:45:39 +0100

    BooleanField requires a default

--- osso/cms/registration/models.py
+++ osso/cms/registration/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class NewsletterRegistration(models.Model):
     user = models.OneToOneField(User)
-    keep_informed = models.BooleanField()
-    active = models.BooleanField()
+    keep_informed = models.BooleanField(default=False)
+    active = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.user.email
