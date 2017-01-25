8155fd61535324e4d8c435a29ccfb76342257abd
Harm Geerts <hgeerts@osso.nl>
2017-01-25 17:05:40 +0100

    Django generic relation imports have moved

--- osso/search/models.py
+++ osso/search/models.py
@@ -1,5 +1,8 @@
 # vim: set ts=8 sw=4 sts=4 et ai:
-from django.contrib.contenttypes import generic
+try:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+except ImportError:
+    from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from osso.search.manager import SearchManager
@@ -19,7 +22,7 @@ class KeywordOccurrence(models.Model):
 
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey()
+    content_object = GenericForeignKey()
 
     objects = SearchManager()
 
