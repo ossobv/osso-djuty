3411d1290923fb33b373ae59161641e91e2f4f51
Harm Geerts <hgeerts@osso.nl>
2017-03-03 15:39:15 +0100

    Make flushdb compatible with Django 1.8

--- osso/core/management/commands/flushdb.py
+++ osso/core/management/commands/flushdb.py
@@ -1,5 +1,9 @@
 # vim: set ts=8 sw=4 sts=4 et ai:
-from optparse import make_option
+import argparse
+import optparse
+import os
+import pwd
+import socket
 import sys
 
 from django import VERSION as django_version
@@ -17,56 +21,60 @@ class Command(BaseCommand):
     __doc__ = help = docstring("""
     Flush the database and recreate it from scratch.
 
-    Implementated by:
+    Implemented by:
      * drop and recreate database
      * syncdb
      * superuser creation
      * config_data insertion
      * example_data insertion
      """)
-
-    if not django_version < (1, 2):
+    # Optparse was used up to Django 1.8.
+    if django_version < (1, 8):
         option_list = BaseCommand.option_list + (
-            make_option(
+            optparse.make_option(
                 '--database', action='store', dest='database',
                 default=DEFAULT_DB_ALIAS,
                 help='Nominates a database to flush. '
                      'Defaults to the "default" database.'),
         )
 
+    def add_arguments(self, parser):
+        parser.formatter_class = argparse.RawTextHelpFormatter
+        parser.add_argument('--database', action='store',
+            default='default', help='Nominates a database to flush. '
+                                    'Defaults to the "default" database.')
+
     def handle(self, *args, **kwargs):
         answer = raw_input('Are you sure you want to flush/reset the db '
                            '[y/n] ? ')
         if answer.strip() != 'y':
-            print('Aborted.')
+            self.stdout.write('Aborted.')
             sys.exit(1)
 
         database = kwargs.get('database', DEFAULT_DB_ALIAS)
 
-        sys.stdout.write('Dropping and recreating database ...')
+        self.stdout.write('Dropping and recreating database ...', ending='')
         utils.recreatedb(database=database)
-        sys.stdout.write('done\n')
+        self.stdout.write('done')
 
-        sys.stdout.write('Creating tables and more syncdb stuff ...')
+        self.stdout.write('Creating tables and more syncdb stuff ...', ending='')
         call_command('syncdb', interactive=False, verbosity=0,
                      database=database)
-        sys.stdout.write('done\n')
+        self.stdout.write('done\n')
 
-        sys.stdout.write('Creating superuser automatically ...')
+        self.stdout.write('Creating superuser automatically ...', ending='')
         username, password, email = self.autosu(database=database)
-        sys.stdout.write('done (%s, %s, %s)\n' % (username, password, email))
+        self.stdout.write('done (%s, %s, %s)' % (username, password, email))
 
-        sys.stdout.write('Loading fixtures: config_data, example_data ...')
+        self.stdout.write('Loading fixtures: config_data, example_data ...',
+                          ending='')
         call_command('loaddata', 'config_data', 'example_data',
                      interactive=False, verbosity=0, database=database)
-        sys.stdout.write('done\n')
+        self.stdout.write('done')
 
     def autosu(self, database=DEFAULT_DB_ALIAS):
         # Non-interactive superuser creation
-        import os
-        import socket
         try:
-            import pwd
             username = pwd.getpwuid(os.getuid())[0].lower()
         except (AttributeError, ImportError):
             # Windows does not have getpwuid nor getuid
