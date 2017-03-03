94a816fc6ce684cc45eccbcb9269c64991834c6c
Harm Geerts <hgeerts@osso.nl>
2017-03-03 14:35:28 +0100

    Make ostat compatible with Django 1.8

--- osso/core/management/commands/ostat.py
+++ osso/core/management/commands/ostat.py
@@ -1,4 +1,5 @@
 # vim: set ts=8 sw=4 sts=4 et ai:
+import argparse
 import optparse
 
 from django import VERSION as django_version
@@ -7,9 +8,12 @@ try:
     from django.contrib.admin.utils import get_deleted_objects
 except ImportError:
     from django.contrib.admin.util import get_deleted_objects
+try:
+    from django.apps import apps
+    get_model = apps.get_model
+except ImportError:
+    from django.db.models import get_model
 from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import models
 from osso.core.management.base import BaseCommand, CommandError, docstring
 
 
@@ -20,34 +24,60 @@ class Command(BaseCommand):
     Supply two+ arguments: app_label.model_name object_pk...
     """)
 
-    option_list = BaseCommand.option_list + (
-        optparse.make_option('--rdepend-count', action='store_true',
-            default=False, help='Print count of dependent objects'),
-    )
+    # Optparse was used up to Django 1.8.
+    if django_version < (1, 8):
+        option_list = BaseCommand.option_list + (
+            optparse.make_option('--rdepend-count', action='store_true',
+                default=False, help='Print count of dependent objects'),
+        )
+
+    missing_args_message = 'invalid/missing arguments, see ostat --help'
+
+    def add_arguments(self, parser):
+        parser.formatter_class = argparse.RawTextHelpFormatter
+        parser.add_argument('model', help='The app_label.model_name')
+        parser.add_argument('args', nargs='+', help='The object primary keys')
+        parser.add_argument('--rdepend-count', action='store_true',
+            default=False, help='Print count of dependent objects')
 
     def handle(self, *args, **kwargs):
+        if 'model' not in kwargs:
+            # Convert from optparse to argparse.
+            if len(args) < 2:
+                raise CommandError(self.missing_args_message)
+            args = list(args)
+            kwargs['model'] = args.pop(0)
+
+        if '.' not in kwargs['model']:
+            raise CommandError('Format model as app_label.model_name')
+
+        app_label, model_name = kwargs['model'].split('.', 1)
+
         try:
-            assert len(args) >= 2 and '.' in args[0], \
-                'Need (only) object type and object pk arguments'
-            app_label, model_name = args[0].split('.', 1)
-            model = models.get_model(app_label, model_name)
-            assert model, ('No model found for app_label %s, model_name %s' %
-                           (app_label, model_name))
-        except (AssertionError, IndexError, ObjectDoesNotExist) as e:
-            if args[0] == 'django.content_type':
+            # Note that old get_model returns None
+            model = get_model(app_label, model_name)
+        except LookupError:
+            model = None
+
+        if model is None:
+            if kwargs['model'] == 'django.content_type':
                 model = ContentType
             else:
-                raise CommandError('No object found: %s' %
-                                   (': '.join(unicode(i) for i in e.args),))
+                raise CommandError('No model found: %s' % kwargs['model'])
 
-        if kwargs.get('rdepend_count', False):
+        if kwargs['rdepend_count']:
             action = self.print_rdepend_count
         else:
             action = self.print_stat
 
-        for object_id in args[1:]:
-            object = model.objects.get(pk=object_id)
-            action(object)
+        for object_id in args:
+            try:
+                object = model.objects.get(pk=object_id)
+            except model.DoesNotExist:
+                self.stderr.write('%r with pk %r does not exist' %
+                                  (model, object_id))
+            else:
+                action(object)
 
     def count_rdepends(self, object):
         deps = self.get_rdepends(object)
@@ -88,10 +118,13 @@ class Command(BaseCommand):
 
                 if django_version >= (1, 3):
                     using = 'default'
-                    (dependent_objects, perms_needed, protected) = \
-                        get_deleted_objects((object,), object._meta,
-                                            AnonymousUser(), AdminSite(),
-                                            using)
+                    retval = get_deleted_objects((object,), object._meta,
+                                                 AnonymousUser(), AdminSite(),
+                                                 using)
+                    if django_version >= (1, 8):
+                        (dependent_objects, _, perms_needed, protected) = retval
+                    else:
+                        (dependent_objects, perms_needed, protected) = retval
                 else:
                     (dependent_objects, perms_needed) = \
                         get_deleted_objects((object,), object._meta,
@@ -130,8 +163,10 @@ class Command(BaseCommand):
                         print('      : %s- %s' % ('  ' * indent, child))
 
         opts = object._meta
+        if hasattr(opts, 'module_name'):  # renamed to model_name
+            opts.model_name = opts.module_name
 
-        identifier = '%s.%s:%s' % (opts.app_label, opts.module_name, object.pk)
+        identifier = '%s.%s:%s' % (opts.app_label, opts.model_name, object.pk)
         print('    ID: %s' % (identifier,))
         print(' Value: %s' % (unicode(object),))
         if hasattr(object, 'created'):
--- osso/core/tests/test_commands.py
+++ osso/core/tests/test_commands.py
@@ -1,10 +1,12 @@
 # vim: set ts=8 sw=4 sts=4 et ai:
-from django.test import SimpleTestCase
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.test import TestCase
 from django.utils.six import StringIO
 
 from mock import patch
 
-from osso.core.management.base import BaseCommand
+from osso.core.management.base import BaseCommand, CommandError
 from osso.core.management.compat import RealOutputWrapper
 
 
@@ -14,7 +16,7 @@ class TestCommand(BaseCommand):
         self.stderr.write('hello err...')
 
 
-class CommandTestCase(SimpleTestCase):
+class CommandTestCase(TestCase):
     def test_kwargs(self):
         out = StringIO()
         cmd = TestCommand()
@@ -36,3 +38,17 @@ class CommandTestCase(SimpleTestCase):
         self.assertTrue(isinstance(cmd.stderr, RealOutputWrapper))
         self.assertFalse(isinstance(cmd.stdout._out, RealOutputWrapper))
         self.assertFalse(isinstance(cmd.stderr._out, RealOutputWrapper))
+
+    @patch('sys.stderr', new_callable=StringIO)
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_ostat(self, stdout, stderr):
+        with self.assertRaisesRegexp(CommandError, 'invalid/missing arguments'):
+            call_command('ostat')
+        with self.assertRaisesRegexp(CommandError, 'No model found'):
+            call_command('ostat', 'auth.Pena', '1')
+        call_command('ostat', 'auth.User', '1')
+        self.assertIn("<class 'django.contrib.auth.models.User'> with pk '1' "
+                      "does not exist", stderr.getvalue())
+        User.objects.create_user(username='ostat')
+        call_command('ostat', 'auth.User', '1')
+        self.assertIn('ID: auth.user:1', stdout.getvalue())
