59ceea894f4b95055c7908b86aa520de500ab828
Walter Doekes <walter+github@wjd.nu>
2018-01-22 14:25:57 +0100

    l10n: Skip Deprecated tests if you're not using them

--- osso/l10n/tests/test_middleware.py
+++ osso/l10n/tests/test_middleware.py
@@ -1,13 +1,19 @@
 from django.conf import settings
 from django.test import TestCase
 from django.utils import translation
+from unittest import skipUnless
 
 
+# Only test it if it's loaded. It is deprecated after all..
+@skipUnless(
+    'osso.l10n.middleware.L10nMiddleware' in settings.MIDDLEWARE_CLASSES,
+    'Only testing L10nMiddleware if it is used')
 class LanguageSelectTest(TestCase):
     ACCEPT_LANGUAGE = 'sv, en-gb;q=0.7, da-dk;q=0.8, nl-nl;q=0.9'
 
     def setUp(self, **kwargs):
         # Store settings that we mess with.
+        self.SETTINGS_LANGUAGES = settings.LANGUAGES
         self.SETTINGS_LANGUAGE_CODE = settings.LANGUAGE_CODE
         try:
             self.SETTINGS_LANGUAGE_CODES = settings.LANGUAGE_CODES
@@ -16,15 +22,19 @@ class LanguageSelectTest(TestCase):
 
     def tearDown(self):
         # Restore messed with settings.
+        settings.LANGUAGES = self.SETTINGS_LANGUAGES
         settings.LANGUAGE_CODE = self.SETTINGS_LANGUAGE_CODE
         settings.LANGUAGE_CODES = self.SETTINGS_LANGUAGE_CODES
 
     def test_single_language(self):
+        settings.LANGUAGES = (
+            ('nl', 'Dutch'), ('sv', 'Swedish'), ('de', 'German'))
         if hasattr(settings, 'LANGUAGE_CODES'):
             settings.LANGUAGE_CODES = ()
         settings.LANGUAGE_CODE = 'sv'
 
-        response = self.client.get('/', HTTP_ACCEPT_LANGUAGE=self.ACCEPT_LANGUAGE)
+        response = self.client.get(
+            '/', HTTP_ACCEPT_LANGUAGE=self.ACCEPT_LANGUAGE)
 
         # The middleware should be disabled because of the single language,
         # ergo it does not set Content-Language. However someone else might.
@@ -38,20 +48,25 @@ class LanguageSelectTest(TestCase):
         settings.LANGUAGE_CODE = 'en'
 
         response = self.client.get('/')  # No ACCEPT_LANGUAGE.
-        self.assertEqual(response._headers.get('content-language', (None, ''))[1], 'da')
+        self.assertEqual(
+            response._headers.get('content-language', (None, ''))[1], 'da')
 
     def test_select_fallback_language(self):
         settings.LANGUAGE_CODES = ('es', 'da', 'de', 'nl', 'fr')
         # Since Django 1.8 LANGUAGE_CODE is loaded as the fallback language.
         settings.LANGUAGE_CODE = 'en'
 
-        response = self.client.get('/', HTTP_ACCEPT_LANGUAGE=self.ACCEPT_LANGUAGE)
-        self.assertEqual(response._headers.get('content-language', (None, ''))[1], 'nl')
+        response = self.client.get(
+            '/', HTTP_ACCEPT_LANGUAGE=self.ACCEPT_LANGUAGE)
+        self.assertEqual(
+            response._headers.get('content-language', (None, ''))[1], 'nl')
 
     def test_select_second_language(self):
         settings.LANGUAGE_CODES = ('es', 'da', 'de', 'fr')
         # Since Django 1.8 LANGUAGE_CODE is loaded as the fallback language.
         settings.LANGUAGE_CODE = 'en'
 
-        response = self.client.get('/', HTTP_ACCEPT_LANGUAGE=self.ACCEPT_LANGUAGE)
-        self.assertEqual(response._headers.get('content-language', (None, ''))[1], 'da')
+        response = self.client.get(
+            '/', HTTP_ACCEPT_LANGUAGE=self.ACCEPT_LANGUAGE)
+        self.assertEqual(
+            response._headers.get('content-language', (None, ''))[1], 'da')
