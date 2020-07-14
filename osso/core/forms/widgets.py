d726f9ecc61ac3b7fe2ac38343ec64536f920339
Harm Geerts <hgeerts@osso.nl>
2020-07-14 12:28:31 +0200

    Update builo_attrs parameters
    build_attrs now takes a dictionary as second argument and no longer
    aceepts **kwargs.

--- osso/core/forms/widgets.py
+++ osso/core/forms/widgets.py
@@ -39,7 +39,7 @@ class CheckboxSelectMultipleWithJS(CheckboxSelectMultiple):
         if value is None:
             value = []
         has_id = attrs and 'id' in attrs
-        final_attrs = self.build_attrs(attrs, name=name)
+        final_attrs = self.build_attrs(attrs, {'name': name})
         output = []
         # Normalize to strings
         str_values = set([force_text(v) for v in value])
