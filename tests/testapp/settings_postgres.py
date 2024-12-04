d03c2fcba2942217e25a460f9de8f1e515850c39
Harm Geerts <hgeerts@osso.nl>
2024-12-04 16:41:49 +0100

    Update tox envlist and fix coverage include

--- tests/testapp/settings_postgres.py
+++ tests/testapp/settings_postgres.py
@@ -1,5 +1,6 @@
 # vim: set ts=8 sw=4 sts=4 et ai:
 # Django settings for testapp project.
+import django
 from .settings import *  # noqa
 
 DATABASES = {
@@ -12,3 +13,9 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+# Django 2.2 with psycopg2 has issues.
+# AssertionError: database connection isn't set to UTC
+# SystemError: type psycopg2.extensions.ReplicationConnection has the Py_TPFLAGS_HAVE_GC flag but has no traverse function
+if django.VERSION < (3, 0):
+    USE_TZ = False
